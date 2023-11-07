ca2d69d8869c44d8784dce2b568677fc25fb6271
SeelyksDev <kevincordier.dev@gmail.com>
2023-11-08 00:37:53 +0100

    Changement d'image fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,45 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 //Récupération des flèches
-let arrows = document.querySelectorAll('#banner .arrow')
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+let i = -1;
 
-// Boucle pour mettre un eventListener sur chacune des flèches
-for (i = 0; i < arrows.length; i++) {
-	arrows[i].addEventListener('click', (event) => {
-		
-	}
-)}
+arrowRight.addEventListener("click", () => {
+  // Récupération de l'id "banner"
+  const banner = document.getElementById("banner");
+  // Création d'un élément "img"
+  const imageAdd = document.createElement("img");
+  //Ajout d'une class "banner-img" à mon élément "img"
+  imageAdd.classList = "banner-img";
+  //Incrémentation de i qui parcours le tableau "slides"
+  i++;
+  // Récupération des valeurs choisi dans le tableau
+  let image = slides[i].image;
+  let tagline = slides[i].tagLine;
+  //Ajout du chemin de l'image à mon élement img
+  imageAdd.src = `./assets/images/slideshow/${image}`;
+  // Ajoute un enfant "img" à "banner"
+  banner.appendChild(imageAdd);
+});
 
- let variable;
-
- const azer = "valeur"
-
- if (azer === "valeur") {
-	console.log("validé");
- } else {
-	console.log("c'est faux");
- }
-
- const array = ["a", "b", "c", "d", "e"]
-
- for (let index = 0; index < array.length; index++) {
-	const element = array[index];
-	console.log(element);
-	watch(element)
- }
-
- function watch (a) {
-	console.log(a);
- }
-
- const title = document.getElementById('title')
- const paragraphe = document.querySelectorAll('.paragraphe')
- const button = document.getElementById('button')
- const form = document.getElementById('form')
-
- form.addEventListener("submit", (event) => {
-	event.preventDefault()
- })
-
- button.addEventListener("click", () => {
-
- })
-
- fetch("http//localhost:8080")
- .then ((response) => {
-	return response.json()
-
- }) 
-
- .then((data) => {
-	console.log(data);
- })
-
-
-
+arrowLeft.addEventListener("click", () => {
+  console.log("it's left");
+});
